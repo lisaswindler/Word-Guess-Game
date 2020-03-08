fcc5d464eada155818986dea4752c5273eb19780
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-03-08 16:17:32 -0600

    adding initial files

---
+++ assets/JavaScript/game.js
@@ -0,0 +1,25 @@
+// Create an array of words to choose from
+
+// Take a random selection from the array
+
+// Display word as underscrores for number of letters in the word
+
+// Create number of guesses allowed (10)
+
+// Add onkey input for all letters of the alphabet (toLowerCase)
+
+// If input matches a letter in the word, fill in that letter (includes)
+
+// If input does not match, add the letter to the already guessed section & subtract one guess
+
+// If the same letter is guessed again, do nothing
+
+// Only allow letters as entries
+
+// If they guess 10 letters with no success, they lose the game (alert)
+
+// If they successfully fill in the word with the correct letters, they win the game (alert)
+
+// Either way, they get a new word to guess
+
+// Continue until all words have been guessed, then game stops
