8cba135e0f6754f8ff17b68621e114591603e367
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-03-09 01:09:18 -0600

    html & css mostly done, js half

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -1,25 +1,77 @@
+// Create variables that reference the HTML so we know where to display things
+var correctLetters = document.getElementById("correct-letters");
+var wrongLetters = document.getElementById("wrong-letters");
+var guessesLeft = document.getElementById("guesses-left");
+var winsText = document.getElementById("wins-text");   
+var lossesText = document.getElementById("losses-text"); 
+var emptySpaces = document.getElementById("empty-spaces");     
 // Create an array of words to choose from
+var seaAnimals = ["sea urchin", "whale shark", "lamprey", "angelfish", 
+"swordfish", "stingray", "octopus", "giant squid", "manatee", "anemone", 
+"barnacle", "cuttlefish", "narwhal", "porpoise", "beluga"];
+// Take a random word from the array
+var seaAnimal = seaAnimals[Math.floor(Math.random() * seaAnimals.length)];
+console.log(seaAnimal);
+//Track number of wins
+var wins = 0;
+// winsText.textContent = wins;
+// Track how many letters are left to be guessed
+var remainingLetters = seaAnimal.length;
+console.log(remainingLetters);
+// Create number of guesses allowed
+var remainingGuesses = 12;
+console.log(remainingGuesses);
+// Turn word into underscores
+var blankArray = [];
+for (var i=0; i < seaAnimal.length; i++)
+blankArray.push('_');
+console.log(blankArray);
+// Track keys pressed
+document.onkeyup = function(event) {
+console.log(event.key);
+// If key pressed is not a letter
+if (event.key !== 'a')
+{alert("Please press a letter")
+// If letter pressed is in word
+} else if (seaAnimal.includes(event.key)) {
+    console.log("Right")
+// If letter is not in word
+} else {
+    console.log("Wrong");
+    remainingGuesses--;
+}
+}
+// Display guess
+// wrongLetters.textContent = guess;
+// // Turn word into underscores
 
-// Take a random selection from the array
-
-// Display word as underscrores for number of letters in the word
-
-// Create number of guesses allowed (10)
-
-// Add onkey input for all letters of the alphabet (toLowerCase)
-
-// If input matches a letter in the word, fill in that letter (includes)
-
-// If input does not match, add the letter to the already guessed section & subtract one guess
-
-// If the same letter is guessed again, do nothing
-
-// Only allow letters as entries
-
-// If they guess 10 letters with no success, they lose the game (alert)
-
-// If they successfully fill in the word with the correct letters, they win the game (alert)
-
-// Either way, they get a new word to guess
-
-// Continue until all words have been guessed, then game stops
+//Display underscores in HTML
+// emptySpaces.textContent = blankArray;
+// var blankArray = document.getElementById("empty-spaces");
+// If input matches a letter in the word, fill in that letter
+// for (var j = 0; j < seaAnimal.length; j++) {
+//     if (seaAnimal[j] === guess) {
+//         emptySpaces[j] = guess;
+//         remainingLetters--;
+// }
+// }
+// When page opens:
+// You see blank spaces and a heading that tells you to press a key
+// while game goes {
+//     Keys pressed are detected    
+//     if not a letter {
+//         return an error} 
+//         if a letter in the word that hasn't been guessed {
+//         replace underscore with letter + decrease remaining letters by 1
+//     }   else if letter pressed is not in word {
+//         add letter to wrong letter display + decrease remaining guesses by 1
+//     }  if letter has already been pressed {
+//         do nothing
+//     } 
+// if all letters are filled in {
+//     congratulate player + show picture of animal + wins++
+// } else if 12 letters are pressed without guessing correctly {
+//     fill in letters + show picture of animal
+// } 
+// repeat process with new word
+// }
