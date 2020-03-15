47def61dac1c86c2d32273a9794c7b1652833302
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-03-14 23:57:42 -0600

    added photos to accompany each word, styling

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -10,10 +10,7 @@ var emptySpaces = document.getElementById("empty-spaces");
 var seaAnimals = ["whale", "shark", "lamprey", "angelfish",
 "swordfish", "stingray", "octopus", "squid", "manatee", "anemone",
 "barnacle", "cuttlefish", "narwhal", "porpoise", "beluga"];
-//    var seaAnimals = ["whale", "shark"];
 
-//var seaAnimal = seaAnimals[Math.floor(Math.random() * seaAnimals.length)];
-//console.log(seaAnimal);
 // Track number of wins
 var wins = 0;
 winsText.textContent = wins;
@@ -22,7 +19,8 @@ winsText.textContent = wins;
 var losses = 0;
 lossesText.textContent = losses;
 
-var remainingGuesses = 12;
+// Set guesses
+var remainingGuesses = 10;
 var numCorrectLetters = 0;
 
 // Create number of guesses allowed
@@ -46,11 +44,11 @@ function makeNewGame() {
     var seaAnimal = seaAnimals[Math.floor(Math.random() * seaAnimals.length)];  
     var positionAnimal = seaAnimals.indexOf(seaAnimal);
     if (seaAnimals.length < 1) {
-            alert("Good job!");
+            alert("Good job!");   
         } else {
             seaAnimals.splice(positionAnimal, 1);
         }     
-    remainingGuesses = 12;
+    remainingGuesses = 10;
     numCorrectLetters = 0;
     correctLetters.textContent = "";
     wrongLetters.textContent = ""
@@ -68,14 +66,12 @@ var allLetters = [];
     // If key pressed is a letter, record the key pressed and store it
         var letter = event.key;
         var letterCode = event.keyCode;
-        
+    
         if (letterCode >= 65 && letterCode <= 90) {
             
             //check if that key was already pressed
             if (allLetters.includes(letter) === false) {
-                allLetters.push(letter);
-                console.log(letter);
-                
+                allLetters.push(letter);                
                 if (randomWord.indexOf(letter) !== -1) {
                     //var addCorrectLetter = letter + ""
                     //correctLetters.textContent += addLetter;
@@ -84,7 +80,6 @@ var allLetters = [];
                     while (pos != -1) {
                         pos = randomWord.indexOf(letter, i + 1);
                         i = pos;
-                        console.log(i);
                         if (i !== -1) {
                             var replaceUnderscore = document.getElementById("underscore_" + i)
                             replaceUnderscore.textContent = " " + letter + " ";
@@ -92,15 +87,47 @@ var allLetters = [];
                         }
                     }
                     if (numCorrectLetters === randomWord.length) {
-                        randomWord = makeNewGame();
-                        wins++;
-                        winsText.textContent = wins;
-                    }
+                        if (randomWord === "whale") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/whale.jpg">';
+                            } else if (randomWord === "shark") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/shark.jpg">';      
+                            } else if (randomWord === "lamprey") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/lamprey.jpg">';  
+                            } else if (randomWord === "angelfish") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/angelfish.jpg">';   
+                            } else if (randomWord === "swordfish") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/swordfish.jpg">';   
+                            } else if (randomWord === "stingray") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/stingray.jpg">';   
+                            } else if (randomWord === "octopus") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/octopus.jpg">';   
+                            } else if (randomWord === "squid") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/squid.jpg">';   
+                            } else if (randomWord === "manatee") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/manatee.jpg">';   
+                            } else if (randomWord === "anemone") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/anemone.jpg">';   
+                            } else if (randomWord === "barnacle") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/barnacle.jpg">';   
+                            } else if (randomWord === "cuttlefish") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/cuttlefish.jpg">';   
+                            } else if (randomWord === "narwhal") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/narwhal.jpg">';   
+                            } else if (randomWord === "porpoise") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/porpoise.jpg">';   
+                            } else if (randomWord === "beluga") {
+                                document.getElementById("image").innerHTML = '<img src="assets/images/beluga.jpg">';   
+                            }
+                            wins++;
+                            winsText.textContent = wins;
+                            setTimeout(function() {randomWord = makeNewGame()}, 100);       
+                        }
                 } else {
                         var addLetter = letter + " ";
                         wrongLetters.innerHTML += addLetter;
                         remainingGuesses--;
                         guessesLeft.textContent = remainingGuesses;
+                        
                         if (remainingGuesses === 0) {
                             randomWord = makeNewGame();
                             losses++;
